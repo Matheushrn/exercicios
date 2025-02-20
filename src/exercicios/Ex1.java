47d1f31dbda9425c03e66796b9dd0abb8dd541c7
Matheus Henrique <112993496+Matheushrn@users.noreply.github.com>
2025-02-20 00:18:32 -0300

    terminado

--- src/exercicios/Ex1.java
+++ src/exercicios/Ex1.java
@@ -1,10 +1,22 @@
 package exercicios;
+import java.util.Scanner;
 
 public class Ex1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite a temperatura em celsius: ");
+		double celsius = sc.nextDouble();
+		
+		double fah = (celsius * 9/5) + 32;
+		
+		double kelvin = celsius + 273.15;
+		
+		System.out.print("Os graus celcius convertido em Fahrenheit fica " + fah + " e em kelvin fica " + kelvin);
+		
+		
+		sc.close();
 	}
 
 }
